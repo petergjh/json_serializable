e1c55584b67d7b39c951765e3f5a8cec34a6be1b
peter <306739889@qq.com>
2020-02-25 08:44:43 +0800

    button

---
+++ json_serializable/Assets/Scripts/test2.cs
@@ -0,0 +1,28 @@
+/* ***********************************************
+* Discribe：
+* Author：PeterGao
+* CreateTime：2020-02-25 08:36:50
+* Edition：1.0
+* ************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using LitJson;
+using System.IO;
+
+public class test2 : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
